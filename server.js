1fbe094922e48e228d12c3027d2d7b8c74f97041
Zaurbek <bigz3733@gmail.com>
2017-01-29 19:22:02 +0000

    first-commit

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express=require("express");
+var bodyParser=require("body-parser")
+var path=require("path")
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use('/assets',express.static('assets'))
+app.get('/:time', function (req, res) {
+  res.send((+ new Date()).toString())
+  console.log(req.url)
+})
+app.get('/',function(req,res) {
+    console.log(req.url)
+    res.sendFile(path.join(__dirname+'/index.html'))
+    
 })
 
+
+
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 8080!')
 })
